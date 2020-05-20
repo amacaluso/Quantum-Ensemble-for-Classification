6d227f83faccca73a2802326ddfdccc5dea67d43
amacaluso <antonio.macaluso@studio.unibo.it>
2020-05-21 01:56:25 +0200

    more experiments ok

--- run_all.py
+++ run_all.py
@@ -4,21 +4,10 @@ from past.builtins import execfile
 # seeds = random.sample(range(1, 10**3), 5)
 # print(seeds)
 # 962, 274, 2, 238, 589, 469, 497, 725, 894, 988, 733, 219, 164, 315, 161
-seeds = [164, 315, 161]
-stds = [.2, .3, .4, .5]
-d_vector = [1]
 
-for seed in seeds:
-    for d in d_vector:
-        for std in stds:
-            n_train = 2**d
-            if n_train > 8:
-                n_train = 8
-            print(seed, d, n_train, std)
-            execfile('quantum_ensemble.py')
 
-seeds = [219, 164, 315, 161]
-stds = [.2, .3, .4, .5]
+seeds = [ 161]
+stds = [.4, .5]
 d_vector = [2]
 
 for seed in seeds:
