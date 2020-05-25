0d46776418cec15912029e0a0e1299c0a46bc7ad
amacaluso <antonio.macaluso@studio.unibo.it>
2020-05-25 20:49:19 +0200

    20 experiments for each combination of d and std

--- run_all.py
+++ run_all.py
@@ -3,10 +3,7 @@ from past.builtins import execfile
 # import random
 # seeds = random.sample(range(10000, 10**5), 5)
 # print(seeds)
-# [962, 274, 2, 238, 589, 469, 497, 725, 894, 988, 733,
-# 219, 164, 315, 161, 68705, 97442, 96367, 28810, 68996]
-
-seeds = [97442, 96367, 28810, 68996]
+seeds = [962, 274, 2, 238, 589, 469, 497, 725, 894, 988, 733, 219, 164, 315, 161, 68705, 97442, 96367, 28810, 68996]
 stds = [.3, .4, .5, .6]
 d_vector = [1, 2, 3, 4]
 
@@ -18,9 +15,3 @@ for seed in seeds:
                 n_train = 8
             print(seed, d, n_train, std)
             execfile('quantum_ensemble.py')
-
-
-# 68705
-# .6
-# 3-8
-# 4-8
