4e66efc47a555a8c6cab6ba75e3e4a3454dc527a
amacaluso <antonio.macaluso@studio.unibo.it>
2020-05-25 11:58:05 +0200

    Till 20 experiments for each std/d/n_train (running)

--- run_all.py
+++ run_all.py
@@ -3,12 +3,12 @@ from past.builtins import execfile
 # import random
 # seeds = random.sample(range(10000, 10**5), 5)
 # print(seeds)
-# 962, 274, 2, 238, 589, 469, 497, 725, 894, 988, 733, 219, 164, 315, 161
+# [962, 274, 2, 238, 589, 469, 497, 725, 894, 988, 733,
+# 219, 164, 315, 161, 68705, 97442, 96367, 28810, 68996]
 
-
-seeds = [962, 274, 2, 238, 589, 469, 497, 725, 894, 988, 733, 219, 164, 315, 161]
-stds = [.6 ] # .2, .3
-d_vector = [1,2, 3, 4]
+seeds = [97442, 96367, 28810, 68996]
+stds = [.3, .4, .5, .6]
+d_vector = [1, 2, 3, 4]
 
 for seed in seeds:
     for d in d_vector:
@@ -19,3 +19,8 @@ for seed in seeds:
             print(seed, d, n_train, std)
             execfile('quantum_ensemble.py')
 
+
+# 68705
+# .6
+# 3-8
+# 4-8
